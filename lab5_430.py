73d14a7df265b493517eb20933ef7f652e2d8f43
Stakhmich Karina <karinastahmic@gmail.com>
2025-11-13 13:38:27 +0200

    111

--- lab5_430.py
+++ lab5_430.py
@@ -1,4 +1,5 @@
-numbers = list(map(int, input("Enter digits separated by spaces: ").split()))
+numbers = list(map(int, input("Enter digits \
+separated by spaces: ").split()))
 n = int(input("Enter the number to determine the amount: "))
 count_n = numbers.count(n)
 print(f"Number {n} occurs {count_n} times.")
