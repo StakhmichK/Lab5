103f204f9fe8777a606388e1b021344ca9c69497
Stakhmich Karina <karinastahmic@gmail.com>
2025-11-15 14:01:13 +0200

    111

--- lab5_456.py
+++ lab5_456.py
@@ -1,4 +1,5 @@
-binary_numbers = input("Enter 4-digit binary numbers separated by commas: ").split(',')
+binary_numbers = input("Enter 4-digit binary numbers\
+separated by commas: ").split(',')
 divisible_by_5 = []
 for b in binary_numbers:
      decimal_value = int(b, 2)
